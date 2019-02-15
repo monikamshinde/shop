0c9054047aa668c6003fdb939982c2072f64d76e
Monika <shindemonika1411@gmail.com>
2019-02-14 19:26:10 -0500

    shopping kart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { ProductdetailsComponent } from './productdetails/productdetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductlistComponent,
+    ProductdetailsComponent
   ],
   imports: [
     BrowserModule,
